74218823ffdbd7aa9ece63935eec1d263856610b
Guinard Arnaud <arnaud.guinard.e@thalesdigital.io>
2024-03-26 17:25:41 +0100

    feat: add tests

--- projects/angular-no-ssr/src/no-ssr.component.spec.ts
+++ projects/angular-no-ssr/src/no-ssr.component.spec.ts
@@ -1,24 +1,80 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, PLATFORM_ID } from '@angular/core';
+import { NoSsrModule } from './no-ssr.module';
 
-import { NoSsrComponent } from './no-ssr.component';
+@Component({
+  template: `
+    <no-ssr>
+      <div id="data">display value</div>
+      <ssr-placeholder>
+        <div id="placeholder">loading</div>
+      </ssr-placeholder>
+    </no-ssr>
+  `
+})
+class NoSsrTestComponent { }
 
-describe('NoSsrComponent', () => {
-  let component: NoSsrComponent;
-  let fixture: ComponentFixture<NoSsrComponent>;
+describe('NoSsrComponent - browser mode', () => {
+  let component: NoSsrTestComponent;
+  let fixture: ComponentFixture<NoSsrTestComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [NoSsrComponent]
+      imports: [NoSsrModule],
+      declarations: [NoSsrTestComponent]
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NoSsrComponent);
+    fixture = TestBed.createComponent(NoSsrTestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("On browser mode, I have data component and I don't have placeholder component", () => {
+
+    const data = fixture.nativeElement.querySelector('#data');
+    expect(data).not.toBeNull();
+    expect(data.textContent).toBe('display value');
+
+    const placeholder = fixture.nativeElement.querySelector('#placeholder');
+    expect(placeholder).toBeNull();
+  });
+});
+
+describe('NoSsrComponent - server mode', () => {
+  let component: NoSsrTestComponent;
+  let fixture: ComponentFixture<NoSsrTestComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [NoSsrModule],
+      declarations: [NoSsrTestComponent],
+      providers: [{ provide: PLATFORM_ID, useValue: 'server' }]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NoSsrTestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("On server mode, I don't have data component and I have placeholder component", () => {
+
+    const data = fixture.nativeElement.querySelector('#data');
+    expect(data).toBeNull();
+
+    const placeholder = fixture.nativeElement.querySelector('#placeholder');
+    expect(placeholder).not.toBeNull();
+    expect(placeholder.textContent).toBe('loading');
   });
 });
